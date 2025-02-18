9e0b08c85984ff2799f2ec4717be0a56437602b7
new_nut <nuttum16@gmail.com>
2025-02-18 23:59:28 +0700

    ข้อที่ 1

---
+++ script.js
@@ -0,0 +1,23 @@
+const userList = document.getElementById('user-list');
+
+fetch('https://jsonplaceholder.typicode.com/users')
+// แปลงข้อมูลที่ได้เป็น JSON เป็น JavaScript Object ด้วย .json()
+    .then(response => response.json())
+    // ข้อมูลอยู่ใน data ทำอะไรต่อกับข้อมูลที่ได้
+    .then(data => {
+        data.forEach(post => {
+            console.log(post);
+            const postElement = document.createElement('p');
+            // postElement.textContent = `${post.name} (${post.email})`;
+            postElement.innerHTML = `
+            <div class="user-list">
+                <h3>${post.name}</h3>
+                <p>${post.email}</p>
+            </div>`;
+
+            userList.appendChild(postElement);
+        })
+    })
+    .catch(error => {
+        console.error('Error fecthing data:', error);
+    });
